e86ac273b3911b2c40aac43003005321aa2a4f82
Gresham Dave <greyashdave@gmail.com>
2024-05-14 15:19:36 +0530

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = ({color, text, text_color}) => {
+  return (
+    <button style={{backgroundColor: color, color: text_color}} className="btn">{text}</button>
+  )
+}
+
+export default Button
